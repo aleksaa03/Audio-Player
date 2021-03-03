f600f2bc23735b96a121a87718ebc28a3ff34d46
Aleksa <71262651+aleksaa03@users.noreply.github.com>
2021-03-03 15:51:12 +0100

    Add files via upload

--- Audio player/app.js
+++ Audio player/app.js
@@ -1,27 +1,63 @@
-var test = document.getElementById("current-time");
 var audio = document.getElementById("audio");
+var library = document.getElementById("library");
 var buttonPlaying = document.getElementById("button-playing");
 
-function startMusic(file) {
-  audio.src = "media/" + file + ".mp3";
-  audio.currentTime = 0;
-  audio.volume = 0.01;
-  audio.play();
+var musicList = [
+  {
+    id: 0,
+    name: "Load Indian Music",
+    file: "media/loud_indian_music.mp3",
+  },
+  {
+    id: 1,
+    name: "Hot",
+    file: "media/hot.mp3",
+  },
+];
+
+for (var i = 0; i < musicList.length; i++) {
+  library.innerHTML += `<div class="music" id="${musicList[i].id}" onclick="startMusic(${musicList[i].id})">
+    <h1>${musicList[i].name}</h1>
+  </div>`;
+}
+
+var musicPlaying = document.getElementById("music-playing");
+
+function startMusic(id) {
+  for (var i = 0; i < musicList.length; i++) {
+    if (musicList[i].id == id) {
+      musicPlaying.innerHTML = musicList[i].name;
+      audio.src = musicList[i].file;
+      audio.currentTime = 0;
+      audio.volume = 0.01;
+      buttonPlaying.className = "fas fa-play";
+      audio.play();
+    }
+  }
 }
 
 var getTime;
+
 buttonPlaying.addEventListener("click", function () {
-  if (this.innerHTML == "Pustanje") {
-    this.innerHTML = "Pauzirano";
+  if (this.className == "fas fa-play") {
+    this.className = "fas fa-pause";
     getTime = audio.currentTime;
     audio.pause();
   } else {
-    this.innerHTML = "Pustanje";
+    this.className = "fas fa-play";
     audio.currentTime = getTime;
     audio.play();
   }
 });
 
-setInterval(function () {
-  console.log(audio.currentTime);
-}, 1000);
+function test(e) {
+  console.log(e.target.files[0]);
+  var getValue = e.target.value;
+  musicList.push({
+    id: 1,
+    name: "Hot",
+    file: "media/" + getValue.replace("C:\\fakepath\\", ""),
+    size: Math.round((e.target.files[0].size / 1048576) * 100) / 100, //to MB
+  });
+  console.log(musicList);
+}
