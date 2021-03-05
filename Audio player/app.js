c57f730c46e563c4fc8a20f6ea25fe7fde85d15f
Aleksa <71262651+aleksaa03@users.noreply.github.com>
2021-03-05 14:25:55 +0100

    Add files via upload

--- Audio player/app.js
+++ Audio player/app.js
@@ -2,26 +2,20 @@ var audio = document.getElementById("audio");
 var library = document.getElementById("library");
 var buttonPlaying = document.getElementById("button-playing");
 
-var musicList = [
-  {
-    id: 0,
-    name: "Load Indian Music",
-    file: "media/loud_indian_music.mp3",
-  },
-  {
-    id: 1,
-    name: "Hot",
-    file: "media/hot.mp3",
-  },
-];
+var musicList = [];
 
 for (var i = 0; i < musicList.length; i++) {
   library.innerHTML += `<div class="music" id="${musicList[i].id}" onclick="startMusic(${musicList[i].id})">
     <h1>${musicList[i].name}</h1>
+    <h4>Size: ${musicList[i].size}MB</h4>
   </div>`;
 }
 
 var musicPlaying = document.getElementById("music-playing");
+
+if (musicPlaying.innerHTML == "") {
+  musicPlaying.innerHTML = "No music is playing";
+}
 
 function startMusic(id) {
   for (var i = 0; i < musicList.length; i++) {
@@ -50,14 +44,46 @@ buttonPlaying.addEventListener("click", function () {
   }
 });
 
-function test(e) {
-  console.log(e.target.files[0]);
-  var getValue = e.target.value;
-  musicList.push({
-    id: 1,
-    name: "Hot",
-    file: "media/" + getValue.replace("C:\\fakepath\\", ""),
-    size: Math.round((e.target.files[0].size / 1048576) * 100) / 100, //to MB
-  });
-  console.log(musicList);
+function getFile(e) {
+  var file = e.target.files[0];
+  fileDetails.innerHTML = `File Name: ${file.name} <br> Size: ${
+    Math.round((file.size / 1024 / 1024) * 100) / 100
+  }MB`;
+  inputName.value = file.name.replace(".mp3", "");
+}
+
+var fileUpload = document.getElementById("file-upload");
+var importFileBcg = document.getElementById("import-file-bcg");
+var fileDetails = document.getElementById("file-details");
+
+function openFile() {
+  fileUpload.click();
+}
+
+var inputName = document.getElementById("name");
+var idCounter = 0;
+
+function saveSong(status) {
+  var file = fileUpload.files[0];
+  if (!status) {
+    if (file != undefined) {
+      musicList.push({
+        id: idCounter++,
+        name: inputName.value,
+        file: "media/" + file.name,
+        size: Math.round((file.size / 1024 / 1024) * 100) / 100,
+      });
+      library.innerHTML = "";
+      for (var i = 0; i < musicList.length; i++) {
+        library.innerHTML += `<div class="music" id="${musicList[i].id}" onclick="startMusic(${musicList[i].id})">
+        <h1>${musicList[i].name}</h1>
+        <h4>Size: ${musicList[i].size}MB</h4>
+      </div>`;
+      }
+      inputName.value = "";
+      importFileBcg.style.display = "none";
+    }
+  } else {
+    importFileBcg.style.display = "block";
+  }
 }
